def7725492a64a3a04f7f8ca438f63641df88996
ebube-tech <83477062+ebube-tech@users.noreply.github.com>
2022-07-11 03:28:16 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+#1. Create a greeting for your program.
+print("Hello Rockstar \U0001f600, Let's find out your band name")
+#2. Ask the user for the city that they grew up in.
+a = input("What is the name of the city you grew up in?")
+#3. Ask the user for the name of a pet.
+b = input("What was the name of your first pet?")
+#4. Combine the name of their city and pet and show them their band name.
+print("Here you go! your bandname is " + a + b)
